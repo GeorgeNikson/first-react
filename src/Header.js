f74c00d8b7a6ec6aaba0d2a26f983a606a5a2ce3
GeorgeNikson <georgiynikwow@gmail.com>
2021-04-27 12:07:43 +0300

    update

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <div>
+            <a href="#s">Home</a>
+            <a href="#s">Messages</a>
+            <a href="#s">News Feed</a>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/Technologies.js
@@ -0,0 +1,14 @@
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>cs</li>
+                <li>htm</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Technologies;
