810c1b1c34808ffc6099eaefa43c4cc5e61cf1a1
GeorgeNikson <georgiynikwow@gmail.com>
2021-06-02 16:00:44 +0300

    added navlink and changed some colors

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,23 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import style from "./NavBar.module.css";
 
 function NavBar() {
     return (
         <nav className={style.nav}>
             <div className={style.item}>
-                <a href="/profile">Profile</a>
+                <NavLink to="/profile" activeClassName={style.active}>Profile</NavLink>
             </div>
             <div className={style.item}>
-                <a href="/news_feed">News feed</a>
+                <NavLink to="/news_feed" activeClassName={style.active}>News feed</NavLink>
             </div>
-            <div className={`${style.item} ${style.active}`}>
-                <a href="/messages">Messages</a>
-            </div>
-            <div className={style.item}>
-                <a href="/music">Music</a>
+            <div className={`${style.item}`}>
+                <NavLink to="/messages" activeClassName={style.active}>Messages</NavLink>
             </div>
             <div className={style.item}>
-                <a href="/settings">Settings</a>
+                <NavLink to="/music" activeClassName={style.active}>Music</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to="/settings" activeClassName={style.active}>Settings</NavLink>
             </div>
         </nav>
     );
